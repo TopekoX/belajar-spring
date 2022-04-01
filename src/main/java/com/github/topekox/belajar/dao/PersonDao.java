3114c6b69a75419df4bc3b6539b1dcd6bcaffd33
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-02 02:01:10 +0800

    re-init belajar spring

--- src/main/java/com/github/topekox/belajar/dao/PersonDao.java
+++
@@ -1,9 +0,0 @@
-package com.github.topekox.belajar.dao;
-
-import com.github.topekox.belajar.entity.Person;
-
-public interface PersonDao {
-    public void save(Person p);
-    
-    public Person findPersonById(Integer id);
-}
