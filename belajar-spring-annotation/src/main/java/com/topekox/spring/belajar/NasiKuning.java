638ece53522bec8832e0471ad4c9faa28da8e295
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-03 23:14:05 +0800

    spring lifecycle Java Configuration no XML

--- belajar-spring-annotation/src/main/java/com/topekox/spring/belajar/NasiKuning.java
+++ belajar-spring-annotation/src/main/java/com/topekox/spring/belajar/NasiKuning.java
@@ -1,17 +1,25 @@
 package com.topekox.spring.belajar;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Value;
 
 public class NasiKuning implements Makanan {
-	
+
 	private MinumanServices minumanServices;
-	
+
 	@Value("${foo.porsi}")
 	private String porsi;
-	
+
 	@Value("${foo.value}")
 	private String value;
-	
+
+	public NasiKuning(MinumanServices minumanServices) {
+		System.out.println(">> NasiKuning : menjalankan constructor");
+		this.minumanServices = minumanServices;
+	}
+
 	public String getPorsi() {
 		return porsi;
 	}
@@ -20,8 +28,16 @@ public class NasiKuning implements Makanan {
 		return value;
 	}
 
-	public NasiKuning(MinumanServices minumanServices) {
-		this.minumanServices = minumanServices;
+	// mendefinisikan init method
+	@PostConstruct
+	public void initMethod() {
+		System.out.println(">> NasiKuning : menjalankan method initMethod()");
+	}
+
+	// mendefinisikan destroy method
+	@PreDestroy
+	public void destroyMethod() {
+		System.out.println(">> NasiKuning : menjalankan method destroyMethod()");
 	}
 
 	@Override
