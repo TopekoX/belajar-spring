0a925cdba9e0f8a69a90370c288f2913bfc8a7d1
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-03 14:37:29 +0800

    ubah jadwal ke Umum

---
+++ belajar-spring-satu/src/main/java/com/tutorialtimposu/spring/belajar/JadwalUmumService.java
@@ -0,0 +1,10 @@
+package com.tutorialtimposu.spring.belajar;
+
+public class JadwalUmumService implements JadwalService {
+
+	@Override
+	public String getJadwal() {
+		return "Hari ini ada pelajaran: Bahasa Indonesia";
+	}
+
+}
