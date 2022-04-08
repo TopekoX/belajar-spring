6d9a611d1f765b76ddf7f040d72f49a1acb79e99
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-09 02:51:19 +0800

    Read Data

--- belajar-hibernate-basic/src/main/java/com/topekox/spring/hibernate/entity/Student.java
+++ belajar-hibernate-basic/src/main/java/com/topekox/spring/hibernate/entity/Student.java
@@ -24,6 +24,10 @@ public class Student {
 	
 	@Column(name="email")
 	private String email;
+	
+	public Student() {
+		
+	}
 
 	public Student(String firstName, String lastName, String email) {
 		this.firstName = firstName;
