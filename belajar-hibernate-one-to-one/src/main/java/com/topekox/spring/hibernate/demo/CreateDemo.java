a54b649fb7e9a79d01e700f542d48358b5d755cb
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-10 00:42:03 +0800

    update Create One To One (rename file)

--- belajar-hibernate-one-to-one/src/main/java/com/topekox/spring/hibernate/demo/CreateDemo.java
+++ belajar-hibernate-one-to-one/src/main/java/com/topekox/spring/hibernate/demo/CreateDemo.java
@@ -7,7 +7,7 @@ import org.hibernate.cfg.Configuration;
 import com.topekox.spring.hibernate.entity.Instructor;
 import com.topekox.spring.hibernate.entity.InstructorDetail;
 
-public class CreateStudentDemo {
+public class CreateDemo {
 
 	public static void main(String[] args) {
 
