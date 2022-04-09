324d279537114078928ff3a7fc88519e00293717
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-09 11:48:48 +0800

    Query hibernate

---
+++ belajar-hibernate-basic/src/main/java/com/topekox/spring/hibernate/demo/QueryStudentDemo.java
@@ -0,0 +1,59 @@
+package com.topekox.spring.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.topekox.spring.hibernate.entity.Student;
+
+public class QueryStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// query student
+			List<Student> theStudent = session.createQuery("from Student").getResultList();
+			
+			// display student
+			displayStudents(theStudent);
+			
+			// query students: lastname Gray
+			theStudent = session.createQuery("from Student s where s.lastName='Gray'").getResultList();
+			
+			// display student
+			System.out.println("\nstudents by last name");
+			displayStudents(theStudent);
+			
+			// commit transaction
+			session.getTransaction().commit();
+		
+			System.out.println("Done");
+			
+		} finally {
+			factory.close();
+		}		
+
+	}
+
+	private static void displayStudents(List<Student> theStudent) {
+		for ( Student student : theStudent ) {
+			System.out.println(student);
+		}
+	}
+
+}
