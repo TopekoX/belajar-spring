1bc4990b2ad551de76fce5f5f310089eed1ae62f
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-09 14:28:12 +0800

    rename Create and Read file

--- belajar-hibernate-basic/src/main/java/com/topekox/spring/hibernate/demo/ReadStudentDemo.java
+++ belajar-hibernate-basic/src/main/java/com/topekox/spring/hibernate/demo/ReadStudentDemo.java
@@ -22,18 +22,38 @@ public class ReadStudentDemo {
 		try {
 			// use the session object to save Java Object
 			System.out.println("create new student object");
-			Student student = new Student("Azwar", "Anas", "azwar@tutorialtimposu.com");
+			Student student = new Student("Moh", "Rizki", "rizky@tutorialtimposu.com");
 			
 			// start a transaction
 			session.beginTransaction();
 			
 			// save the student object
 			System.out.println("Saving the student");
+			System.out.println(student);
+			
 			session.save(student);
 			
 			// commit transaction
 			session.getTransaction().commit();
 			
+			// find out the student id: primary key
+			System.out.println("Saved student. Generated id: " + student.getId());		
+			
+			// now get new session & start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			// retrive student base on id: primary key
+			System.out.println("\nGetting student whit id: " + student.getId());
+			
+			Student myStudent = session.get(Student.class, student.getId());
+			
+			System.out.println("Complete!!! : " + myStudent);
+			
+			// commit the transaction
+			session.getTransaction().commit();
+						
+			// find out student id: primary key
 			System.out.println("Done");
 			
 		} finally {
