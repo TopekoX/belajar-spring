1f22dd3537b32a688425cc74f98200504a962049
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-12 00:06:55 +0800

    ManyToMany Get Course for Student

---
+++ belajar-hibernate-many-to-many/src/main/java/com/topekox/spring/hibernate/demo/GetCourseForMaryDemo.java
@@ -0,0 +1,68 @@
+package com.topekox.spring.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.topekox.spring.hibernate.entity.Course;
+import com.topekox.spring.hibernate.entity.Instructor;
+import com.topekox.spring.hibernate.entity.InstructorDetail;
+import com.topekox.spring.hibernate.entity.Review;
+import com.topekox.spring.hibernate.entity.Student;
+
+public class GetCourseForMaryDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class)
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// start a transaction
+			session.beginTransaction();
+
+			// get the student mary from databases
+			int id = 2;
+			Student student = session.get(Student.class, id);
+			
+			System.out.println("\nLoaded student: " + student);
+			System.out.println("Courses: " + student.getCourses());
+			
+			// create more courses
+			Course course1 = new Course("Rubik's Cube - How to Speed Cube");
+			Course course2 = new Course("Atari 2600 - Game Development");
+			
+			// add student to course
+			course1.addStudent(student);
+			course2.addStudent(student);
+	
+			// save student to course
+			System.out.println("\nSaving the Course");
+			
+			session.save(course1);
+			session.save(course2);
+			
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!!!");
+
+		} finally {
+			// add clean up
+			session.close();
+			
+			factory.close();
+		}
+
+	}
+
+}
