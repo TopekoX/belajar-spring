44a0f24aceba7903bc3a9f506d35455059839d7c
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-12 00:41:47 +0800

    ManyToMany Delete Student

---
+++ belajar-hibernate-many-to-many/src/main/java/com/topekox/spring/hibernate/demo/DeleteMaryStudentDemo.java
@@ -0,0 +1,59 @@
+package com.topekox.spring.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.topekox.spring.hibernate.entity.Course;
+import com.topekox.spring.hibernate.entity.Instructor;
+import com.topekox.spring.hibernate.entity.InstructorDetail;
+import com.topekox.spring.hibernate.entity.Review;
+import com.topekox.spring.hibernate.entity.Student;
+
+public class DeleteMaryStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class)
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// start a transaction
+			session.beginTransaction();
+
+			// get the student mary from databases
+			int id = 2;
+			Student student = session.get(Student.class, id);
+			
+			System.out.println("\nLoaded student: " + student);
+			System.out.println("Courses: " + student.getCourses());
+			
+			// delete Student
+			System.out.println("\nDelete Student: " + student);
+			
+			session.delete(student);
+			
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!!!");
+
+		} finally {
+			// add clean up
+			session.close();
+			
+			factory.close();
+		}
+
+	}
+
+}
