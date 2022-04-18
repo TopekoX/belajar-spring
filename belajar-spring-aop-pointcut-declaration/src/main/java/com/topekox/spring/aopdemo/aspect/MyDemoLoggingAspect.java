c4fca4ed25128f51300eafaab3ef87c986673b3d
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-18 21:10:34 +0800

    @Pointcut Declaration

--- belajar-spring-aop-pointcut-declaration/src/main/java/com/topekox/spring/aopdemo/aspect/MyDemoLoggingAspect.java
+++ belajar-spring-aop-pointcut-declaration/src/main/java/com/topekox/spring/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.topekox.spring.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,9 +13,18 @@ public class MyDemoLoggingAspect {
 	
 	// lets start with an @Before Advice
 	
-	@Before("execution(* com.topekox.spring.aopdemo.dao.*.*(..))")
+	@Pointcut("execution(* com.topekox.spring.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {		
+	}
+	
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>>> Execution @Before advice on account()");
+	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====>>> Perform API analytics");
 	}
 
 }
