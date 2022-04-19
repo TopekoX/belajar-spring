3740497372621d35fe3d7ef9fd10938abc6d811d
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-20 06:12:16 +0800

    ini @After Finnaly

---
+++ belajar-spring-aop-after-finally/src/main/java/com/topekox/spring/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -0,0 +1,18 @@
+package com.topekox.spring.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(1)
+public class MyCloudLogAsyncAspect {
+	
+	@Before("com.topekox.spring.aopdemo.aspect.TopekoxAopExpressions.forDaoPackageNoGetterSetter()")
+	public void logToCloudAsync() {
+		System.out.println("\n=====>>> Logging to Cloud in async fashion");
+	}
+
+}
