dba67d69507dac95e7e9bb902551ec3d4b706363
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-20 08:47:01 +0800

    Spring AOP Around

---
+++ belajar-spring-aop-around/src/main/java/com/topekox/spring/aopdemo/AfterReturningDemoApp.java
@@ -0,0 +1,44 @@
+package com.topekox.spring.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.topekox.spring.aopdemo.dao.AccountDAO;
+
+public class AfterReturningDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring containner
+		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// call method find account
+		List<Account> accounts = null;
+		
+		try {
+			// add boolean flag to simulate exception
+			boolean tripWare = true;
+			accountDAO.findAccounts(tripWare);
+		} catch (Exception e) {
+			System.out.println("\nMain Program... caught exception : " + e);
+		}
+		
+		// display the account
+		System.out.println("\n\nMain Program: After Throwing Demo App");
+		System.out.println("------");
+		
+		System.out.println(accounts);
+		
+		System.out.println("\n");
+			
+		// close the context
+		context.close();
+
+	}
+
+}
