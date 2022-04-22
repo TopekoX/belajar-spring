abce28bb770120e0ff832fea3603c0f5c78c22b0
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-22 13:50:53 +0800

    init custom login form spring security

---
+++ belajar-spring-security-custom-form-login/src/main/java/com/topekox/belajar/spring/security/config/SecurityWebConfig.java
@@ -0,0 +1,25 @@
+package com.topekox.belajar.spring.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("ucup").password("123456").roles("EMPLOYEE"))
+			.withUser(users.username("azwar").password("1234567").roles("MANAGER"))
+			.withUser(users.username("aprizal").password("12345678").roles("EMPLOYEE"));
+	}
+
+}
