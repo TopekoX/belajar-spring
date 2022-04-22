4276ddcaf380fcef0c797a44f8a0858b0e49d9e3
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-22 14:47:41 +0800

    add Custom Login Spring Security

--- belajar-spring-security-custom-form-login/src/main/java/com/topekox/belajar/spring/security/config/SecurityWebConfig.java
+++ belajar-spring-security-custom-form-login/src/main/java/com/topekox/belajar/spring/security/config/SecurityWebConfig.java
@@ -2,6 +2,7 @@ package com.topekox.belajar.spring.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -20,6 +21,18 @@ public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
 			.withUser(users.username("ucup").password("123456").roles("EMPLOYEE"))
 			.withUser(users.username("azwar").password("1234567").roles("MANAGER"))
 			.withUser(users.username("aprizal").password("12345678").roles("EMPLOYEE"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/loginPage")
+				.loginProcessingUrl("/authenticateUser")
+				.permitAll();
 	}
 
 }
