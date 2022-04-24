b4755db4b45fd324485dcf38cf551dc9daa05e8c
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-24 16:29:34 +0800

    init belajar spring json data binding

---
+++ belajar-spring-json-data-binding/src/main/java/com/topekox/spring/json/App.java
@@ -0,0 +1,13 @@
+package com.topekox.spring.json;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
