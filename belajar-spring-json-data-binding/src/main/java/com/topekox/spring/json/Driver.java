8fbde6c9d45a856f36a18b1a7302a431500a1ce3
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-24 17:35:32 +0800

    read data nested and array

--- belajar-spring-json-data-binding/src/main/java/com/topekox/spring/json/Driver.java
+++ belajar-spring-json-data-binding/src/main/java/com/topekox/spring/json/Driver.java
@@ -3,6 +3,7 @@ package com.topekox.spring.json;
 import java.io.File;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.topekox.spring.json.entity.Address;
 import com.topekox.spring.json.entity.Student;
 
 public class Driver {
@@ -16,11 +17,22 @@ public class Driver {
 			// read JSON file and map/convert to Java POJO
 			// sample-lite.json
 			Student student = 
-					objectMapper.readValue(new File("data/sample-lite.json"), Student.class);
+					objectMapper.readValue(new File("data/sample-full.json"), Student.class);
 			
 			// print out
 			System.out.println("First name = " + student.getFirstName());
 			System.out.println("Last name = " + student.getLastName());
+			
+			// print address
+			Address address = student.getAddress();
+			
+			System.out.println("Street = " + address.getStreet());
+			System.out.println("City = " + address.getCity());
+			
+			// print language
+			for (String lang : student.getLanguages()) {
+				System.out.println(lang);
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
--- belajar-spring-json-data-binding/src/main/java/com/topekox/spring/json/entity/Student.java
+++ belajar-spring-json-data-binding/src/main/java/com/topekox/spring/json/entity/Student.java
@@ -7,6 +7,10 @@ public class Student {
 	private String lastName;
 	private boolean active;
 	
+	private Address address;
+	
+	private String[] languages;
+	
 	public Student() {
 		
 	}
@@ -43,4 +47,20 @@ public class Student {
 		this.active = active;
 	}
 
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String[] getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(String[] languages) {
+		this.languages = languages;
+	}
+	
 }
