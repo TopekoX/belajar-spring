e0d4692edb9c44f04e1c6cced1c3b0e0e0bef81e
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-26 19:45:16 +0800

    Spring REST ADD Customer

--- belajar-spring-rest-crud/src/main/java/com/topekox/spring/rest/crud/rest/CustomerRestController.java
+++ belajar-spring-rest-crud/src/main/java/com/topekox/spring/rest/crud/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +34,15 @@ public class CustomerRestController {
 		if (customer == null) {
 			throw new CustomerNotFoundException("Customer ID Not Found - " + idCustumer);
 		}
+		
+		return customer;
+	}
+	
+	@PostMapping("/customer")
+	public Customer addCustomer(@RequestBody Customer customer) {		
+		
+		customer.setId(0);
+		customerService.saveCustomer(customer);
 		
 		return customer;
 	}
