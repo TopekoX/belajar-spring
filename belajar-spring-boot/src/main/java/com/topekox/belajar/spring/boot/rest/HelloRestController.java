b94ef48e7c928cfa20c619ab9f98279ef4a9944d
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-27 22:39:34 +0800

    add rest index url

---
+++ belajar-spring-boot/src/main/java/com/topekox/belajar/spring/boot/rest/HelloRestController.java
@@ -0,0 +1,16 @@
+package com.topekox.belajar.spring.boot.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World!!! Time server is = " + LocalDateTime.now();
+	}
+	
+}
