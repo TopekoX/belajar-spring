a16bb47981f5d215f8c26a884dfbe584500c893d
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-28 00:59:13 +0800

    init spring boot actuator

---
+++ belajar-spring-boot-actuator/src/main/java/com/topekox/belajar/spring/boot/BelajarSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.topekox.belajar.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringBootApplication.class, args);
+	}
+
+}
