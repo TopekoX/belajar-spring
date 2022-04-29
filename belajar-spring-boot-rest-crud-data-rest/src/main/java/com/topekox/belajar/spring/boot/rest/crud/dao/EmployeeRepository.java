76ad7ec81fec22d9ebca539c065321dee7e8ec68
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-29 21:00:18 +0800

    spring boot data rest custom url by repository

--- belajar-spring-boot-rest-crud-data-rest/src/main/java/com/topekox/belajar/spring/boot/rest/crud/dao/EmployeeRepository.java
+++ belajar-spring-boot-rest-crud-data-rest/src/main/java/com/topekox/belajar/spring/boot/rest/crud/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.topekox.belajar.spring.boot.rest.crud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.topekox.belajar.spring.boot.rest.crud.entity.Employee;
 
+@RepositoryRestResource(path = "pegawai")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
