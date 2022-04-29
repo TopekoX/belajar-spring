696c0376122e5bf3302fb6cf710342230f939ed9
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-29 22:25:43 +0800

    init spring boot thymeleaf

---
+++ belajar-spring-boot-thymeleaf/src/test/java/com/topekox/belajar/spring/boot/thymeleaf/BelajarSpringBootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topekox.belajar.spring.boot.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringBootThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
