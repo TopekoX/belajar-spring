0cf268c686b2e036cd287a716fac4f190b051713
Ucup TopekoX <ucup.topekox@gmail.com>
2022-04-30 23:37:22 +0800

    add employee spring boot thymeleaf

--- belajar-spring-boot-thymeleaf-list-crud-db/src/main/java/com/topekox/belajar/spring/boot/thymeleaf/controller/EmployeeController.java
+++ belajar-spring-boot-thymeleaf-list-crud-db/src/main/java/com/topekox/belajar/spring/boot/thymeleaf/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.topekox.belajar.spring.boot.thymeleaf.entity.Employee;
@@ -15,7 +17,7 @@ import com.topekox.belajar.spring.boot.thymeleaf.service.EmployeeService;
 public class EmployeeController {
 
 	private EmployeeService employeeService;
-	
+
 	public EmployeeController(EmployeeService employeeService) {
 		this.employeeService = employeeService;
 	}
@@ -24,8 +26,24 @@ public class EmployeeController {
 	public String listEmployee(Model model) {
 		List<Employee> listEmployees = employeeService.findAll();
 		model.addAttribute("employees", listEmployees);
+
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showAddForm")
+	public String showAddForm(Model model) {
+		Employee theEmployee = new Employee();
+		model.addAttribute("employee", theEmployee);
+
+		return "employees/form-employees";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute Employee employee) {
+		employeeService.save(employee);
 		
-		return "list-employees";
+		// redirect
+		return "redirect:/employee/list";
 	}
 
 }
--- belajar-spring-boot-thymeleaf-list-crud-db/src/main/java/com/topekox/belajar/spring/boot/thymeleaf/dao/EmployeeRepository.java
+++ belajar-spring-boot-thymeleaf-list-crud-db/src/main/java/com/topekox/belajar/spring/boot/thymeleaf/dao/EmployeeRepository.java
@@ -1,9 +1,15 @@
 package com.topekox.belajar.spring.boot.thymeleaf.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.topekox.belajar.spring.boot.thymeleaf.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	// add method sort by last name by Spring JPA Magic
+	// search more by keyword "spring data jpa query methods"
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
 }
